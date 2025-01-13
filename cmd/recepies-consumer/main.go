7c57a98930ce30f29b7bfff529c78b7574686972
Donskov <donskov_a@icloud.com>
2025-01-13 16:34:53 +0500

    cleaning main

--- cmd/recepies-consumer/main.go
+++ cmd/recepies-consumer/main.go
@@ -1,33 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/IBM/sarama"
-	"os"
-	"strings"
-)
-
 func main() {
-	config := sarama.NewConfig()
-
-	config.Consumer.Return.Errors = true
-	brokers := os.Getenv("KAFKA_BROKERS")
-	consumer, err := sarama.NewConsumer(strings.Split(brokers, ","), config)
-	if err != nil {
-		panic(err)
-	}
-	defer consumer.Close()
-	partitionConsumer, err := consumer.ConsumePartition("internal.KafkaTopicCreateRecipes", 0, sarama.OffsetNewest)
-	if err != nil {
-		panic(err)
-	}
-	defer partitionConsumer.Close()
-	messages := partitionConsumer.Messages()
-	for {
-		select {
-		case msg := <-messages:
-			fmt.Printf("Message claimed :%s \n", string(msg.Value))
-		}
-	}
 
 }
